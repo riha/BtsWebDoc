3b45ebd242fbd54e4a2f5d3ec522ace65363e856
Richard Hallgren <richard.hallgren@gmail.com>
2012-11-19 22:04:50 +0100

    Fixed filter expression conversions
    Fixes issue #10

--- btswebdoc.Model/Filter.cs
+++ btswebdoc.Model/Filter.cs
@@ -10,5 +10,29 @@ namespace btswebdoc.Model
         public string Value { get; set; }
 
         public string FilterOperator { get; set; }
+
+        public static string FilterOperatorToSign(string filterOperator)
+        {
+            if (filterOperator == "0")
+                return "==";
+
+            if (filterOperator == "1")
+                return "<";
+
+
+            if (filterOperator == "2")
+                return "<=";
+
+            if (filterOperator == "3")
+                return ">";
+
+            if (filterOperator == "4")
+                return ">=";
+
+            if (filterOperator == "5")
+                return "!=";
+
+            return "Exists";
+        }
     }
 }
