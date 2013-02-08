538b890dda959e89bbab1ddb03662b3efb8acea9
Richard Hallgren <richard.hallgren@gmail.com>
2013-02-08 16:51:11 +0100

    Adding info on send and receive handlers in web

---
+++ btswebdoc.Web/Models/TransportInfoModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using btswebdoc.Model;
+
+namespace btswebdoc.Web.Models
+{
+    public class TransportInfoModel
+    {
+        public TransportInfo TransportInfo { get; set; }
+        public Manifest CurrentManifest { get; set; }
+
+    }
+}
